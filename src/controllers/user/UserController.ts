46daa3d48f0cfb4f754ac7dc7112ab92e419758e
Jackson Ming Hu <huming2207@gmail.com>
2020-04-04 07:46:24 +1100

    Add mongoose connection

--- src/controllers/user/UserController.ts
+++ src/controllers/user/UserController.ts
@@ -1,14 +1,27 @@
-import { BaseController } from '../BaseController';
 import { Context, Next } from 'koa';
-import koaBody from 'koa-body';
-
+import { BaseController } from '../BaseController';
+import { Joi } from 'koa-joi-router';
 
 export class UserController extends BaseController {
     constructor() {
         super('/user');
+        this.router.post('', {
+            validate: {
+                body: {
+                    username: Joi.string().max(50),
+                    password: Joi.string().max(50),
+                    email: Joi.string().email(),
+                },
+            },
+        });
     }
 
-    private get = async (ctx: Context, next: Next): Promise<void> => {
-
+    private createUser = async (ctx: Context, next: Next): Promise<void> => {
+        const body = ctx.request.body;
+        const userName = body['username'];
+        const passwordText = body['password'];
+        const email = body['email'];
+        
+        
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import logger from 'koa-logger';
+import * as mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 const app = new Koa();
 const router = new Router();
