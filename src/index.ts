51c9a1ac58c125e35a06eda92ffcf8c45f046bcd
Jackson Ming Hu <huming2207@gmail.com>
2020-04-05 09:23:10 +1000

    Add dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,27 @@ import Koa from 'koa';
 import logger from 'koa-logger';
 import mongoose from 'mongoose';
 import { UserController } from './controllers/user/UserController';
+import jwt from 'koa-jwt';
 
-mongoose.connect('mongodb://localhost:27017/test', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+require('dotenv').config();
+
+mongoose.connect(
+    process.env.SYNCIFY_DB_URL ? process.env.SYNCIFY_DB_URL : 'mongodb://localhost:27017/syncify',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+);
 
 const app = new Koa();
 const userController = new UserController();
 
 app.use(logger());
+app.use(
+    jwt({
+        secret: process.env.SYNCIFY_JWT_SECRET ? process.env.SYNCIFY_JWT_SECRET : 'jwtSecret',
+        algorithms: ['HS512'],
+    }),
+);
 app.use(userController.router.middleware());
 app.listen(3000);
