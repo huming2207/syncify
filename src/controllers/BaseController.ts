1a8a92f95bccd589223216253d8ee5789d161bca
Jackson Ming Hu <huming2207@gmail.com>
2020-04-10 21:47:20 +1000

    Add unauthorised handler middleware

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -1,4 +1,5 @@
 import * as koaJoiRouter from 'koa-joi-router';
+import { Context, Next } from 'koa';
 
 export abstract class BaseController {
     private _router: koaJoiRouter.Router;
@@ -15,4 +16,17 @@ export abstract class BaseController {
         this._router = koaJoiRouter.default();
         this._router.prefix('/api');
     }
+}
+
+export async function UnauthorisedHandler(ctx: Context, next: Next): Promise<void> {
+    return next().catch((err) => {
+        if (err.status !== 401) throw err;
+        if (ctx.state.jwtOriginalError || !ctx.state.user) {
+            ctx.status = 401;
+            ctx.type = 'json';
+            ctx.body = { msg: "You haven't logged in!", data: err };
+        } else {
+            throw err;
+        }
+    });
 }
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import logger from 'koa-logger';
 import { UserController } from './controllers/UserController';
 import jwt from 'koa-jwt';
 import { connectToDb } from './common/Database';
+import { UnauthorisedHandler } from './controllers/BaseController';
 
 require('dotenv').config();
 
@@ -10,11 +11,13 @@ const app = new Koa();
 const userController = new UserController();
 
 app.use(logger());
+app.use(UnauthorisedHandler);
+
 app.use(
     jwt({
         secret: process.env.SYNCIFY_JWT_SECRET ? process.env.SYNCIFY_JWT_SECRET : 'jwtTestSecret',
         algorithms: ['HS512'],
-        passthrough: true,
+        passthrough: false,
     }).unless({ path: [/^\/api\/user\/register/, /^\/api\/user\/login/] }),
 );
 app.use(userController.router.middleware());
