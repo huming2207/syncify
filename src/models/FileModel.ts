bb74d862c834f91b1a01cd9e5a81818a7813677c
Jackson Ming Hu <huming2207@gmail.com>
2020-04-13 21:58:19 +1000

    file: model: code cleanup

--- src/models/FileModel.ts
+++ src/models/FileModel.ts
@@ -2,16 +2,11 @@ import { Types, Document, Schema, model } from 'mongoose';
 import { UserDoc } from './UserModel';
 import { PathDoc } from './PathModel';
 
-export interface FileMetadata {
-    hash: string;
-    type: string;
-    owner: Types.ObjectId;
-}
-
 export interface FileDoc extends Document {
     size: number;
     hash: string;
     type: string;
+    name: string;
     owner: UserDoc;
     path: PathDoc;
     gridFile: Types.ObjectId;
@@ -21,6 +16,7 @@ export const FileSchema = new Schema({
     size: { type: Number },
     hash: { type: String },
     type: { type: String },
+    name: { type: String },
     owner: { type: Types.ObjectId, ref: 'User' },
     path: { type: Types.ObjectId, ref: 'Path' },
     gridFile: { type: Types.ObjectId },
