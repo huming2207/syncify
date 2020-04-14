e71c5d15ad1643ba0da08ae08a3e80deffbf7d67
Jackson Ming Hu <huming2207@gmail.com>
2020-04-14 21:44:21 +1000

    file: model: also delete the files in GridFS if necessary

--- src/models/FileModel.ts
+++ src/models/FileModel.ts
@@ -1,4 +1,5 @@
-import { Types, Document, Schema, model } from 'mongoose';
+import { Types, Document, Schema, model, NativeError } from 'mongoose';
+import mongodb from 'mongodb';
 import { UserDoc } from './UserModel';
 import { PathDoc } from './PathModel';
 
@@ -20,6 +21,27 @@ export const FileSchema = new Schema({
     owner: { type: Types.ObjectId, ref: 'User' },
     path: { type: Types.ObjectId, ref: 'Path' },
     gridFile: { type: Types.ObjectId },
+});
+
+FileSchema.post('remove', function (
+    err: NativeError,
+    doc: FileDoc,
+    next: (err?: NativeError) => void,
+) {
+    if (err) {
+        next(err);
+        return;
+    }
+
+    if (doc.gridFile) {
+        const db = doc.db.db;
+        const bucket = new mongodb.GridFSBucket(db);
+        bucket.delete(doc.gridFile, (err) => {
+            if (err) next(err);
+        });
+    }
+
+    next();
 });
 
 export default model<FileDoc>('File', FileSchema);
