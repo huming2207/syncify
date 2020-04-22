018d6b8d093830efa3f80be7dbd17f9f5a3362fc
Jackson Ming Hu <huming2207@gmail.com>
2020-04-22 21:01:10 +1000

    Add fastify type helpers

---
+++ src/FastifyTypes.ts
@@ -0,0 +1,26 @@
+import http from 'http';
+import fastify from 'fastify';
+
+declare module 'fastify' {
+    export type ServerInstance = fastify.FastifyInstance<
+        http.Server,
+        http.IncomingMessage,
+        http.ServerResponse
+    >;
+
+    export type ServerRequest = fastify.FastifyRequest;
+    export type MiddlewareOptions = RegisterOptions<
+        http.Server,
+        http.IncomingMessage,
+        http.ServerResponse
+    >;
+
+    export type ServerReply = fastify.FastifyReply<http.ServerResponse>;
+    export type ServerPlugin = Plugin<
+        http.Server,
+        http.IncomingMessage,
+        http.ServerResponse,
+        MiddlewareOptions,
+        Function
+    >;
+}
