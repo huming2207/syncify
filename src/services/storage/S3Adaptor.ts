3ac9a0a24a8a6b377effd55af80e9c152752a7aa
Jackson Ming Hu <huming2207@gmail.com>
2020-04-27 22:11:33 +1000

    service: storage: init

---
+++ src/services/storage/S3Adaptor.ts
@@ -0,0 +1,32 @@
+import { StorageAdaptor } from './StorageAdaptor';
+import { Readable } from 'stream';
+
+export class S3Adaptor implements StorageAdaptor {
+    public performStoreObject = async (
+        bucketName: string,
+        fileName: string,
+        stream: Readable,
+        size: number,
+    ): Promise<string> => {
+        throw new Error('Method not implemented.');
+    };
+
+    public performRetrieveObject = async (
+        bucketName: string,
+        fileName: string,
+    ): Promise<Readable> => {
+        throw new Error('Method not implemented.');
+    };
+
+    public performCopyObject = async (
+        bucketName: string,
+        srcName: string,
+        dstName: string,
+    ): Promise<void> => {
+        throw new Error('Method not implemented.');
+    };
+
+    public performDeleteObject = async (bucketName: string, fileName: string): Promise<void> => {
+        throw new Error('Method not implemented.');
+    };
+}
---
+++ src/services/storage/StorageAdaptor.ts
@@ -0,0 +1,14 @@
+import { Readable } from 'stream';
+
+export interface StorageAdaptor {
+    performStoreObject(
+        bucketName: string,
+        fileName: string,
+        stream: Readable,
+        size: number,
+    ): Promise<string>;
+
+    performRetrieveObject(bucketName: string, fileName: string): Promise<Readable>;
+    performCopyObject(bucketName: string, srcName: string, dstName: string): Promise<void>;
+    performDeleteObject(bucketName: string, fileName: string): Promise<void>;
+}
